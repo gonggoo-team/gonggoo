af27e45ef339868795354c767433831b2e40b947
cheonwooo <cjsgusdn14@naver.com>
2025-08-17 18:52:10 +0900

    init: spring boot 프로젝트 추가

---
+++ src/main/java/com/gonggoo/gonggoo/GonggooApplication.java
@@ -0,0 +1,13 @@
+package com.gonggoo.gonggoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonggoo/gonggoo/GonggooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonggoo.gonggoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
