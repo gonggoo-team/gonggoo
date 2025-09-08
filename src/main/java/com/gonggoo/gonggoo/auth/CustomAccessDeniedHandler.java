21377a5c2b671d1dc7707e75c12926ae09d065e5
cheonwooo <cjsgusdn14@naver.com>
2025-09-08 15:53:51 +0900

    feat: 예외 처리 분리 기능 추가

---
+++ src/main/java/com/gonggoo/gonggoo/auth/CustomAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package com.gonggoo.gonggoo.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest req,
+                       HttpServletResponse res,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        res.setContentType("application/json;charset=UTF-8");
+        res.getWriter().write("""
+                {
+                    "code":"FORBIDDEN",
+                    "message":"권한이 부족합니다."
+                }
+                """);
+    }
+}
---
+++ src/main/java/com/gonggoo/gonggoo/auth/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.gonggoo.gonggoo.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest req,
+                         HttpServletResponse res,
+                         AuthenticationException authException) throws IOException {
+        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        res.setContentType("application/json;charset=UTF-8");
+        res.getWriter().write("""
+                {
+                    "code":"UNAUTHORIZED",
+                    "message":"토큰이 유효하지 않습니다."
+                }
+                """);
+    }
+
+
+}
