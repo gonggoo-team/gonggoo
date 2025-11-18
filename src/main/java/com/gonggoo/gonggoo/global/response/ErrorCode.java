9814494692ef78e96a033d0bafd6a6a297e374da
3veryday <hyunseo.3veryday.ji@gmail.com>
2025-11-18 17:34:47 +0900

    refactor : ErrorCode에 COOPOST_NOT_FOUND 추가

--- src/main/java/com/gonggoo/gonggoo/global/response/ErrorCode.java
+++ src/main/java/com/gonggoo/gonggoo/global/response/ErrorCode.java
@@ -17,8 +17,8 @@ public enum ErrorCode {
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 액세스 토큰입니다.", "만료된 액세스 토큰입니다."),
     NO_AUTHORITY(HttpStatus.FORBIDDEN, "권한 정보가 없는 토큰입니다.", "권한 정보가 없는 토큰입니다."),
     INVALID_AUTH_HEADER(HttpStatus.BAD_REQUEST, "Authorization 헤더가 유효하지 않습니다.", "Authorization 헤더가 유효하지 않습니다."),
-    BLACKLISTED_TOKEN(HttpStatus.UNAUTHORIZED, "BLACKLISTED_TOKEN", "블랙리스트에 포함된 토큰입니다."),;
-
+    BLACKLISTED_TOKEN(HttpStatus.UNAUTHORIZED, "BLACKLISTED_TOKEN", "블랙리스트에 포함된 토큰입니다."),
+    COOPOST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 공구글이 존재하지 않습니다.", "Coopost 엔티티가 db 안에 존재하지 않습니다");
     private final HttpStatus httpStatus;
     private final String message;
     private final String reason;
