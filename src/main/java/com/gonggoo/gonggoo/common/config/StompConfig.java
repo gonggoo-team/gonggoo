1a7cd32d1e7dec12bb643d5ae6c0e72027ae6964
cheonwooo <cjsgusdn14@naver.com>
2025-12-24 17:02:17 +0900

    feat: Token을 가진 유저가 보내는 메세지임을 확인하는 로직 구현

--- src/main/java/com/gonggoo/gonggoo/common/config/StompConfig.java
+++ src/main/java/com/gonggoo/gonggoo/common/config/StompConfig.java
@@ -1,18 +1,39 @@
 package com.gonggoo.gonggoo.common.config;
 
+import com.gonggoo.gonggoo.auth.jwt.JwtTokenProvider;
+import com.gonggoo.gonggoo.global.handler.StompErrorHandler;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("*");
+        registry.setErrorHandler(new StompErrorHandler());
     }
 
     @Override
@@ -21,5 +42,27 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/sub");
         //어플리케이션에 요청을 보낼 때 작성해야 하는 접두사
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String token = getToken(accessor);
+                    log.info("token : {}", token);
+                    jwtTokenProvider.validateToken(token);
+                }
+                return message;
+            }
+        });
+    }
+
+    private String getToken(StompHeaderAccessor accessor) {
+        List<String> nativeHeader = accessor.getNativeHeader("Authorization");
+
+        return nativeHeader == null || nativeHeader.size() != 1 ? null : nativeHeader.get(0);
     }
 }
