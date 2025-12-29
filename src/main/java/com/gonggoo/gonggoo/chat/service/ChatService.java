b78bff7cf27bb450b6d0956baf9e444adf026679
cheonwooo <cjsgusdn14@naver.com>
2025-12-29 12:52:07 +0900

    feat: 커서 기반 채팅 로그 찾는 기능 구현

--- src/main/java/com/gonggoo/gonggoo/chat/service/ChatService.java
+++ src/main/java/com/gonggoo/gonggoo/chat/service/ChatService.java
@@ -6,7 +6,9 @@ import com.gonggoo.gonggoo.chat.dto.MessageSendingDto;
 import com.gonggoo.gonggoo.chat.dto.response.MessageResponse;
 import com.gonggoo.gonggoo.chat.respository.ChatLogRepository;
 import com.gonggoo.gonggoo.chat.respository.ChatroomRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.bridge.Message;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,5 +43,19 @@ public class ChatService {
                 .memberId(messageSendingDto.memberId())
                 .content(messageSendingDto.content())
                 .build();
+    }
+
+    @Transactional(readOnly = true)
+    public List<MessageResponse> findChatLog(Long chatroomId, Long cursor) {
+        List<ChatLog> chatLogs = cursor == null
+                ? chatLogRepository.findTop20ByChatroomIdOrderByIdDesc(chatroomId)//cursor가 null 이면 최초 접속
+                : chatLogRepository.findTop20ByChatroomIdAndIdLessThanOrderByDesc(chatroomId, cursor);//cursor가 null이 아니면 커서값 기반 페이지네이션
+
+        return chatLogs.stream().map(
+                chatLog -> MessageResponse.of(
+                        chatLog.getId(),
+                        chatLog.getMemberId(),
+                        chatLog.getContent(),
+                        chatLog.getCreatedAt())).toList();
     }
 }
