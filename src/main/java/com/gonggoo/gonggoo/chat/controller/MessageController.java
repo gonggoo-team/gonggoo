169ad4f075e167911ae7c521f130e87add67dd4b
cheonwooo <cjsgusdn14@naver.com>
2026-01-02 15:56:36 +0900

    feat: 채팅 로그 보내는 메서드 재구현 - @SendToUser 방식에서 convertAndSendToUser로 재구현

--- src/main/java/com/gonggoo/gonggoo/chat/controller/MessageController.java
+++ src/main/java/com/gonggoo/gonggoo/chat/controller/MessageController.java
@@ -3,18 +3,14 @@ package com.gonggoo.gonggoo.chat.controller;
 import com.gonggoo.gonggoo.chat.dto.request.MessageRequest;
 import com.gonggoo.gonggoo.chat.dto.response.MessageResponse;
 import com.gonggoo.gonggoo.chat.service.ChatService;
-import jakarta.annotation.Nullable;
+import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
@@ -31,10 +27,16 @@ public class MessageController {
         messagingTemplate.convertAndSend("/sub/chat/" + chatroomId, savedMessage);
     }
 
-    @MessageMapping("/chat/{chatroom-id}/log")
-    @SendToUser("/queue/chat/log")
-    public List<MessageResponse> sendChatLog(@DestinationVariable("chatroom-id") Long chatroomId,
-                                             @Header(name = "cursor", required = false) Long cursor) {
-        return chatService.findChatLog(chatroomId, cursor);
+    @MessageMapping("/chat/{chatroomId}/log")
+    public void sendChatLog(@DestinationVariable Long chatroomId,
+                                             @Header(name = "cursor", required = false) Long cursor,
+                                             Principal principal) {
+        List<MessageResponse> chatLogs = chatService.findChatLog(chatroomId, cursor);
+
+        messagingTemplate.convertAndSendToUser(
+                principal.getName(),
+                "/queue/chat/" + chatroomId + "/log",
+                chatLogs
+        );
     }
 }
