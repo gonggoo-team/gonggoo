902d702821deecd2391eb45826d579121dee252a
cheonwooo <cjsgusdn14@naver.com>
2026-02-07 21:17:38 +0900

    feat: fcm 설정 객체 생성

--- src/main/java/com/gonggoo/gonggoo/common/config/FcmConfig.java
+++ src/main/java/com/gonggoo/gonggoo/common/config/FcmConfig.java
@@ -1,32 +1,32 @@
 package com.gonggoo.gonggoo.common.config;
 
+import com.gonggoo.gonggoo.fcm.FcmProps;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
-import lombok.val;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
+@EnableConfigurationProperties(FcmProps.class)
 public class FcmConfig {
 
     private final ClassPathResource firebaseResource;
     private final String projectId;
 
-    public FcmConfig(@Value("${fcm.file_path}") String filePath,
-                     @Value("${fcm.project_id}") String projectId) {
-        this.firebaseResource = new ClassPathResource(filePath);
-        this.projectId = projectId;
+    public FcmConfig(FcmProps fcmProps) {
+        this.firebaseResource = new ClassPathResource(fcmProps.file_path());
+        this.projectId = fcmProps.project_id();
     }
 
     @PostConstruct
     public void init() throws IOException {
-        val option = FirebaseOptions.builder()
+        FirebaseOptions option = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(firebaseResource.getInputStream()))
                 .setProjectId(projectId)
                 .build();
