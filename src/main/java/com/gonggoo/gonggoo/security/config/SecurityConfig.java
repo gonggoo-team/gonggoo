f4f528303352422464b91090615ca50a3a015cd7
cheonwooo <cjsgusdn14@naver.com>
2026-02-26 15:38:43 +0900

    refactor: SpringSecurity 리팩토링 - 주석 삭제 - 허용 url 상수 처리 - 함수 분리

--- src/main/java/com/gonggoo/gonggoo/security/config/SecurityConfig.java
+++ src/main/java/com/gonggoo/gonggoo/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.gonggoo.gonggoo.security.handler.CustomAccessDeniedHandler;
 import com.gonggoo.gonggoo.security.handler.CustomAuthenticationEntryPoint;
 import com.gonggoo.gonggoo.security.handler.CustomLogoutHandler;
 import com.gonggoo.gonggoo.security.writer.SecurityErrorResponseWriter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +26,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private static final String[] PERMIT_URLS = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html",
+            "/ws/**",
+            "/api/v1/notification/**",
+            "/api/device/**",
+            "/api/auth/**",
+            "/api/coopost/v1/**",
+    };
+    private static final String[] PERMIT_POST_METHOD_URLS = {
+            "/api/member/v1/signup",
+            "/api/auth/v1/login"
+    };
+    private static final String[] PERMIT_GET_METHOD_URLS = {
+            "/api/member/v1/check-email/**",
+            "/api/member/v1/check-nickname/**"
+    };
 
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisTokenBlackListService redisTokenBlackListService;
@@ -39,22 +60,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-//                .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers("/swagger-ui.html").permitAll()
-                        .requestMatchers("/ws/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/member/v1/signup").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/member/v1/check-email/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/member/v1/check-nickname/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/v1/login").permitAll()
-                        .requestMatchers("/api/v1/notification/**").permitAll()
-                        .requestMatchers("/api/device/**").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/coopost/v1").permitAll()
-                        .requestMatchers("/api/coopost/v1/**").permitAll()
+                        .requestMatchers(PERMIT_URLS).permitAll()
+                        .requestMatchers(HttpMethod.POST, PERMIT_POST_METHOD_URLS).permitAll()
+                        .requestMatchers(HttpMethod.GET, PERMIT_GET_METHOD_URLS).permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/member/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated())
@@ -66,7 +76,7 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/api/auth/v1/logout")
                         .addLogoutHandler(customLogoutHandler)
-                        .logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK)))
+                        .logoutSuccessHandler(this::handleLogoutSuccess))
                 .build();
     }
 
@@ -75,4 +85,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    private void handleLogoutSuccess(HttpServletRequest req, HttpServletResponse res, Authentication auth) {
+        res.setStatus(HttpServletResponse.SC_OK);
+    }
 }
